b4c120d1b6ef42de629b0a07a933b3dcf24558ee
Alex W <106714485+alex-wang-13@users.noreply.github.com>
2023-05-01 00:55:54 -0400

    Added auto-chinese generation, syntax highlighting
    Used the chat gpt api

--- translate.php
+++ translate.php
@@ -7,30 +7,66 @@
   <title>Translate!</title>
 </head>
 
-<body>
+<body class="bg-dark text-light">
   <div class="container mt-4">
-    <h3>Give some Chinese text:</h3>
+    <h3>Chinese text:</h3>
     <!-- A textbox for user input -->
     <div class="mb-4">
       <form method="POST">
         <div class="form-group">
-          <label for="textbox">Enter text:</label>
-          <textarea name="text" id="textbox" class="form-control" rows="5" cols="50"></textarea>
+          <label for="textbox">Enter text or automatically generate:</label>
+          <?php
+            session_start();
+            // code to generate chinese text
+            if (isset($_POST['generate'])) {
+              // retrieve the api key for chat-gpt
+              $key = trim(file_get_contents('../../../api-key.txt'));
+              // define prompt
+              $prompt = 'Give an example paragraph from a Chinese newspaper. Include no English in your response.';
+
+              // shell command to access chat-gpt
+              $cmd = 'curl https://api.openai.com/v1/chat/completions -H "Content-Type: application/json" -H "Authorization: Bearer ' . $key . '" -d \'{ "model": "gpt-3.5-turbo", "messages": [{"role": "user", "content": "' . $prompt . '"}], "temperature": 0.7 }\'';
+
+              // get the response from the api
+              $response = shell_exec($cmd);
+
+              // decode the json response from api + extract chat-gpt's output
+              $arr = json_decode($response, true, 512, 0);
+              $content = $arr['choices']['0']['message']['content'];
+
+              // display populated textarea
+              echo '<textarea name="text" id="textbox" class="form-control" rows="5" cols="50">' . $content . '</textarea>';
+              $_SESSION['input'] = $content;
+              echo $_SESSION['input'];
+            } else {
+              // use previously input to populate textarea
+              echo '<textarea name="text" id="textbox" class="form-control" rows="5" cols="50">' . $_SESSION['input'] . '</textarea>';
+            }
+          ?>
         </div>
+        <br/>
         <button type="submit" name="translate" class="btn btn-primary">Translate</button>
+        <button type="submit" name="generate" class="btn btn-info">Generate Chinese</button>
       </form>
     </div>
     <!-- A form to enter Chinese text and its English translations as key-value pairs -->
     <div class="mb-4">
       <form method="POST">
-        <div class="form-group">
-          <label for="key">Enter Chinese Character:</label>
-          <input type="text" name="key" id="key" class="form-control">
+        <div class="row">
+        <div class="col-6">
+          <div class="form-group">
+            <label for="key">Enter Chinese Character:</label>
+            <input type="text" name="key" id="key" class="form-control">
+          </div>
         </div>
-        <div class="form-group">
-          <label for="value">Enter Translation:</label>
-          <input type="text" name="value" id="value" class="form-control">
+        <div class="col-6">
+          <div class="form-group">
+            <label for="value">Enter Translation:</label>
+            <input type="text" name="value" id="value" class="form-control">
+          </div>
         </div>
+        </div>
+        <br/>
         <!-- Add the translation to the dictionary -->
         <button type="submit" name="submit" class="btn btn-success">Store in Dictionary</button>
         <!-- Clear the dictionary -->
@@ -42,88 +78,88 @@
       session_start();
 
       // initialize the dictionary
-      if (!isset($_SESSION["dictionary"])) {
-        $_SESSION["dictionary"] = array();
+      if (!isset($_SESSION['dictionary'])) {
+        $_SESSION['dictionary'] = array();
       }
 
       // initailize input sentence
-      if (!isset($_SESSION["input"])) {
-        $_SESSION["input"] = "Enter a sentence in Chinese.";
+      if (!isset($_SESSION['input'])) {
+        $_SESSION['input'] = '';
       }
 
       // if _POST["submit"] is initialized, validate the entries
-      if (isset($_POST["submit"])) {
+      if (isset($_POST['submit'])) {
         // get the key and value
-        $key = $_POST["key"];
-        $value = $_POST["value"];
+        $key = $_POST['key'];
+        $value = $_POST['value'];
         // validate the key and value; the key should only have chinese characters and the value only non-chinese
-        if (preg_match("/^[\x{4e00}-\x{9fff}]$/u", $key) && !preg_match("/^[\x{4e00}-\x{9fff}]+$/u", $value)) {
-          $dictionary = $_SESSION["dictionary"];
-          $dictionary[$key] = $value;
+        if (preg_match('/^[\x{4e00}-\x{9fff}]$/u', $key) && !preg_match('/^[\x{4e00}-\x{9fff}]+$/u', $value)) {
+          $dictionary = $_SESSION['dictionary'];
+          $dictionary[$key] = trim(strtolower($value));
           // update the dictionary
-          $_SESSION["dictionary"] = $dictionary;
+          $_SESSION['dictionary'] = $dictionary;
         } else {
-          echo "INVALID ENTRY / NOTHING STORED. <br>";
+          echo '<p class="text-light">INVALID ENTRY / NOTHING STORED.</p>';
         }
       }
 
       // if _POST["reset"] then reinitialize the dictionary
-      if (isset($_POST["reset"])) {
-        $_SESSION["dictionary"] = array();
+      if (isset($_POST['reset'])) {
+        $_SESSION['dictionary'] = array();
       }
 
       // show the dictionary
-      echo "<hr><h4>Dictionary:</h4>";
-      echo "<pre>";
-      print_r($_SESSION["dictionary"]);
-      echo "</pre>";
-      echo "<hr><h4>Translated Text:</h4>";
+      echo '<hr><h4>Dictionary:</h4>';
+      echo '<pre>';
+      print_r($_SESSION['dictionary']);
+      echo '</pre>';
+      echo '<hr><h4>Translated Text:</h4>';
 
       // verify that the dictionary and _POST variables are initialized
-      if (isset($_SESSION["dictionary"])) { //&& isset($_POST["translate"])) {
+      if (isset($_SESSION['dictionary'])) { //&& isset($_POST['translate'])) {
         // grab the global dictionary variable
-        $dictionary = $_SESSION["dictionary"];
+        $dictionary = $_SESSION['dictionary'];
         // grab the text from _POST or _SESSION
-        $text = $_SESSION["input"];
-        if (isset($_POST["translate"])) {
-          $text = $_POST["text"];
-          $_SESSION["input"] = $text;
+        $text = $_SESSION['input'];
+        if (isset($_POST['translate'])) {
+          $text = $_POST['text'];
+          $_SESSION['input'] = $text;
         }
         // split the text into an array of graphemes, i.e. language units
         // since utf-8 characters have variable lengths, split by //s to get all graphenes
         foreach (preg_split('//u', $text, null, PREG_SPLIT_NO_EMPTY) as $grapheme) {
           // if the grapheme is in the dictionary, then replace it with its translation
           if (isset($dictionary[$grapheme])) {
-            $text = mb_ereg_replace($grapheme, "[" . $dictionary[$grapheme] . "]", $text);
+            $text = mb_ereg_replace($grapheme, '<span style="background-color: green;">[' . $dictionary[$grapheme] . ']</span>', $text);
           }
         }
       }
       // prints the translation
       print_r($text);
-      echo "<hr>";
+      echo '<hr>';
     ?>
     </div>
   </div>
   <footer
-    class="bg-dark text-center text-lg-start"
-    style="background-color: #212121; color: white"
+    class="text-center text-lg-start text-light"
+    style="background-color: black;"
   >
     <div class="container p-4">
       <div class="col-sm-4 col-12">
         <h5 class="text-uppercase">Contact</h5>
         <ul class="list-unstyled">
           <li>
-            <a href="mailto:alex.wang4@case.edu" target="_blank">Email</a>
+            <a href="mailto:alex.wang4@case.edu" target="_blank" style="color: white;">Email</a>
           </li>
           <li>
-            <a href="https://github.com/alex-wang-13" target="_blank"
+            <a href="https://github.com/alex-wang-13" target="_blank" style="color: white;"
               >Github</a
             >
           </li>
           <li>
             <a
               href="https://www.linkedin.com/in/alex-wang-0525b4217/"
-              target="_blank"
+              target="_blank" style="color: white;"
               >LinkedIn</a
             >
           </li>
